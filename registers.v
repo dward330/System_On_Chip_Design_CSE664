828fff31bbc765249776be57ad8e51da213be369
Derrick <dward330@gmail.com>
2023-06-15 21:08:08 -0400

    Got 16 slot register working

--- registers.v
+++ registers.v
@@ -1,3 +1,5 @@
+
+// Module to store up to 16 numbers that are 8 bits long
 module register_unit (reset, clock, load, store, load_addr, store_addr, data_out, data_in);
 
 //register size
@@ -14,47 +16,33 @@ reg [register_size-1:0] datatogoout;
 
 output [register_size-1:0] data_out;
 
-input [register_size-1:0] data_in;
+input wire [register_size-1:0] data_in;
 
-input clock, reset, load, store;
+input wire clock, reset, load, store;
 
-input [3:0] load_addr;
+input wire[3:0] load_addr;
 
-input [3:0] store_addr;
+input wire[3:0] store_addr;
 
+//Only run when we detect a positive clock edge rise
+always @(posedge clock) begin
 
-always @(posedge clock or negedge reset) begin
-
-if (reset==0) begin
-
-	
+// Reset all register slots back to 0 as well as output value being returned to the outside
+if (reset==1) begin
 	for (i = 0; i < register_count; i = i+1) begin
-
-	registers[i] <= 0;
-
+		registers[i] <= 0;
 	end
-
-
+	datatogoout <= 0;
 end
 
+// Fetch data stored from internal register slot and return it to the outside
+if (reset == 0 && load == 1) begin
+	datatogoout <= registers[load_addr];
 end
 
-always @(posedge clock) begin
-
-if (reset == 1 && load == 1) begin
-
-		registers[load_addr] = data_in;
-
-end
-
-end
-
-always @(posedge clock) begin
-
-if (reset == 1 && store == 1) begin
-
-		datatogoout= registers[store_addr];
-
+// Store data from the outside into an internal register slot
+if (reset == 0 && store == 1) begin
+	registers[store_addr] <= data_in;
 end
 
 end
