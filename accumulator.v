1c661d53ce5af517664c1c548dfebc693343f36e
mdotkkdotp-syr <mkpena@syr.edu>
2023-06-19 00:16:06 -0400

    updated report headers and accumulator section

--- accumulator.v
+++ accumulator.v
@@ -8,8 +8,8 @@
 **
 ** DESCRIPTION:
 ** A register in which intermediate arithmetic logic unit results are stored.
-** This simple 8-bit accumulator's output will continuously grow by increments of
-** the input until reset.
+** The 8-bit accumulator's output will grow when directed to update or
+** the input receives the reset signal.
 **
 ********************************************************************************
 ** VERSION HISTORY
@@ -24,47 +24,14 @@
 **
 *******************************************************************************/
 
-/*******************************************************************************
-**
-** v1.0
-**
-*******************************************************************************/
-
 `timescale 1ns / 1ps
-
-// Description
-//! A register in which intermediate arithmetic logic unit results are stored.
-module acc_simple (
-	output [7 : 0] out, //! Value stored in the register, to be returned to ALU.
-	input [7 : 0] in, //! Value (from ALU) to increment current register value by.
-	input clock, //! Clock signal (posedge triggered).
-	input reset //! Signal (active high) to clear register value and set value stored back to 0.
-	);
-
-// ----- Accumulator -----
-	reg [7 : 0] accumulator = 8'b00000000;	// Temporary storage for accumulator output
-
-
-// Increments output by input at rising edge of clock.
-// If reset is high then clear output. 
-
-always @(posedge clock or posedge reset)	// at posedge of clock or reset=1
-begin
-	if (reset)				// if reset = 1
-		accumulator <= 0;		// reset accumulator to 0
-	else
-		accumulator <= accumulator + in; // otherwise, store value in accumulator
-end
-	assign out = accumulator; 		// send value back to ALU
-endmodule
 
 /*******************************************************************************
 **
 ** v1.1
 **
 *******************************************************************************/
-// Description
-//! 
+
 module acc (
 	output [7 : 0] out, //! Value stored in the register, to be returned to ALU.
 	input [7 : 0] in, //! Value (from ALU) to increment current register value by.
@@ -89,3 +56,34 @@ begin
 end
 	assign out = accumulator; 		// send value back to ALU
 endmodule
+
+/*******************************************************************************
+**
+** v1.0
+**
+*******************************************************************************/
+
+module acc_simple (
+	output [7 : 0] out, //! Value stored in the register, to be returned to ALU.
+	input [7 : 0] in, //! Value (from ALU) to increment current register value by.
+	input clock, //! Clock signal (posedge triggered).
+	input reset //! Signal (active high) to clear register value and set value stored back to 0.
+	);
+
+// ----- Accumulator -----
+	reg [7 : 0] accumulator = 8'b00000000;	// Temporary storage for accumulator output
+
+
+// Increments output by input at rising edge of clock.
+// If reset is high then clear output. 
+
+always @(posedge clock or posedge reset)	// at posedge of clock or reset=1
+begin
+	if (reset)				// if reset = 1
+		accumulator <= 0;		// reset accumulator to 0
+	else
+		accumulator <= accumulator + in; // otherwise, store value in accumulator
+end
+	assign out = accumulator; 		// send value back to ALU
+endmodule
+
